b537bf6af4a96a18b0141c53fcb6906cc36c09a4
James Stevenson <james.l.stevenson@gmail.com>
2020-05-14 16:26:22 +0100

    Updating topic float from float32 to float.

--- topic_modelling/topic_modelling.py
+++ topic_modelling/topic_modelling.py
@@ -124,11 +124,10 @@ class topic_modelling:
         topics = ldamodel.get_document_topics(
             new_doc_bow)  # Here we use the LDA object we've trained and provide the new document to get it's topics - These probabilities add up to 1
 
-        # Update for fixing 'flaot32' error. Where the result of this function were not JSON serializable.
         for topic_set in topics:
             topics.remove(topic_set)
             topic = topic_set[0]
-            topic_score = format(round(float(topic_set[1]), 6))
+            topic_score = float(topic_set[1])
             topics.append((topic,topic_score))
 
         return topics
